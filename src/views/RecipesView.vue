5b4080c1fc8b60701d946834ae304e2cfb5fe644
btharding <btharding2014@hotmail.com>
2022-05-29 14:33:49 +0200

    Basic 'Add Recipe' functionality

--- src/views/RecipesView.vue
+++ src/views/RecipesView.vue
@@ -1,8 +1,31 @@
 <template>
     <div>
-        <h1>New Recipe</h1>
-        <h2 v-if="recipes.length">Recipes found</h2>
-        <h2 v-else>No recipes found</h2>
+        <h1>Recipes</h1>
+        <h2>New recipe:</h2>
+        <form @submit="submit">
+            <label for = "recipeName">Recipe Name: </label>
+            <input name = "recipeName" type = "text" v-model="currentRecipe.name" required/><br/>
+
+            <label for = "newIngredient">Ingredients (comma separated): </label>
+            <input name = "newIngredient" type = "text" v-model="currentIngredient" @input="handleIngredientChange"/>
+
+            <ul v-if="currentRecipe.ingredients">
+                <li v-for="ingredient in currentRecipe.ingredients" :key = "ingredient.id">{{ingredient.name}}</li>
+            </ul><br/>
+
+            <input type = "submit" value="Submit"/>
+        </form>
+
+        <div v-if = "recipes.length">
+            <h3>Recipes:</h3>
+
+            <div v-for = "recipe in recipes" :key="recipe.id">
+                <h3>{{recipe.name}}</h3>
+                <span v-for = "ingredient in recipe.ingredients" :key = "ingredient.id" :style = "{color: '#' + ingredient.id.slice(7)}">{{ingredient.name}}</span>
+            </div>
+        </div>
+
+        <h3 v-else>No recipes found</h3>
     </div>
 </template>
 
@@ -10,12 +33,63 @@
     export default {
         data () {
             return {
-                recipes: []
+                currentRecipe: {
+                },
+                currentIngredient: '',
+                recipes: [],
+                ingredients: [],
             }
         },
         mounted () {
             if (localStorage.recipes) {
-                this.recipes = localStorage.recipes
+                this.recipes = JSON.parse(localStorage.recipes);
+            }
+            if (localStorage.ingredients) {
+                this.ingredients = JSON.parse(localStorage.ingredients);
+            }
+        },
+        methods: {
+            generateUniqueId(existingArray) {
+                while (true) {
+                    let candidateId = Math.random().toString(16).slice(2);
+                    if (!existingArray.filter(item => item.id == candidateId).length) {
+                        return candidateId;
+                    }
+                }
+            },
+            submit(e) {
+                e.preventDefault();
+                this.currentRecipe.id = this.generateUniqueId(this.recipes);
+
+                this.recipes.push(this.currentRecipe);
+                localStorage.recipes = JSON.stringify(this.recipes);
+                localStorage.ingredients = JSON.stringify(this.ingredients);
+                this.currentRecipe = {};
+                this.currentIngredient = '';
+            },
+            addNewIngredient(ingredientName) {
+                if (!this.currentRecipe.ingredients) {
+                    this.currentRecipe.ingredients = [];
+                }
+                let matchingIngredients = this.ingredients.filter(ingredient => ingredient.name.toLowerCase() == ingredientName.toLowerCase());
+
+                let ingredient = null;
+                if (matchingIngredients.length) {
+                    ingredient = matchingIngredients[0];
+                } else {
+                    ingredient = {
+                        'name': ingredientName,
+                        'id': this.generateUniqueId(this.recipes)
+                    }
+                    this.ingredients.push(ingredient);
+                }
+                this.currentRecipe.ingredients.push(ingredient);
+            },
+            handleIngredientChange() {
+                if (this.currentIngredient.slice(-1) === ',') {
+                    this.addNewIngredient(this.currentIngredient.slice(0,-1));
+                    this.currentIngredient = '';
+                }
             }
         }
     }
