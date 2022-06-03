24190bd8b16fbd1b44c50a340402d9e01496dc0e
btharding <btharding2014@hotmail.com>
2022-06-03 17:37:39 +0200

    Add script folder

---
+++ src/scripts/DataHandler.js
@@ -0,0 +1,29 @@
+import Helpers from "./Helpers";
+
+export default {
+    fetchRecipes() {
+        if (localStorage.recipes) {
+            return JSON.parse(localStorage.recipes);
+        }
+        return [];
+    },
+    saveRecipes(recipes) {
+        localStorage.recipes = JSON.stringify(recipes);
+    },
+    addRecipe(recipe, recipes) {
+        if ("id" in recipe) {
+            if (recipe.ingredients.length) {
+                recipes = recipes.map((candidateRecipe) => candidateRecipe.id == recipe.id ? recipe : candidateRecipe);
+            } else {
+                recipes = recipes.filter((candidateRecipe) => candidateRecipe.id !== recipe.id);
+            }
+        } else {
+            recipe.id = Helpers.generateUniqueId(recipes);
+            recipes.push(recipe);
+        }
+        //TODO: For each ingredient, add ingredient if new
+        //TODO: Index this recipe by all ingredients
+        this.saveRecipes(recipes);
+        return recipes;
+    }
+}
